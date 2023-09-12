3b7df916c225544669f9ec40da55b87d5036b434
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-11 22:59:37 -0300

    Start project

---
+++ src/test/java/com/example/picpaydesafiobackend/PicpayDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picpaydesafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
