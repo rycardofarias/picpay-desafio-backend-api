33b808b21569d974db383321b19eb20613748433
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-13 00:55:39 -0300

    Create ExceptionMessages

---
+++ src/main/java/com/example/picpay/exceptions/messages/ExceptionMessages.java
@@ -0,0 +1,20 @@
+package com.example.picpay.exceptions.messages;
+
+public class ExceptionMessages {
+
+    public static final String USER_NOT_AUTHORIZED = "The merchant type user is not authorized to carry out transactions.";
+
+    public static final String USER_CANNOT_BE_NULL = "User cannot be null.";
+
+    public static final String USER_NOT_FOUND = "User not found with ID: ";
+
+    public static final String INVALID_USER_ID = "Invalid user ID: ";
+
+    public static final String INSUFFICIENT_FOUNDS = "Insufficient funds.";
+
+    public static final String USER_EMAIL_ALREADY_REGISTERED = "E-mail already registered: ";
+
+    public static final String USER_DOCUMENT_ALREADY_REGISTERED = "Document already registered: ";
+
+    public static final String USER_DOCUMENT_INVALID = "Document invalid: ";
+}
