3fcb6d668cdae8002024aaf35a9a3df6524d667c
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-16 19:08:43 -0300

    Fixing document validation

--- src/main/java/com/example/picpay/validation/documents/DocumentValid.java
+++ src/main/java/com/example/picpay/validation/documents/DocumentValid.java
@@ -2,15 +2,28 @@ package com.example.picpay.validation.documents;
 
 public class DocumentValid {
 
-    public static boolean isCPFValid(String cpf) {
-        // Removes all non-digit characters
-        cpf = cpf.replaceAll("[^0-9]", "");
+    private static String removeNonDigits(String input) {
+        return input.replaceAll("[^0-9]", "");
+    }
 
-        // CPF's formed by a sequence of equal numbers are considered errors
-        if (cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222") ||
-                cpf.equals("33333333333") || cpf.equals("44444444444") || cpf.equals("55555555555") ||
-                cpf.equals("66666666666") || cpf.equals("77777777777") || cpf.equals("88888888888") ||
-                cpf.equals("99999999999") || (cpf.length() != 11)) {
+    private static boolean hasRepeatedDigits(String input) {
+        String pattern = "^(\\d)\\1+$";
+        return input.matches(pattern);
+    }
+
+    private static int calculateDigit(int[] numbers, int[] factors) {
+        int sum = 0;
+        for (int i = 0; i < factors.length; i++) {
+            sum += numbers[i] * factors[i];
+        }
+        int remainder = sum % 11;
+        return (remainder < 2) ? 0 : (11 - remainder);
+    }
+
+    public static boolean isCPFValid(String cpf) {
+        cpf = removeNonDigits(cpf);
+
+        if (cpf.length() != 11 || hasRepeatedDigits(cpf)) {
             throw new IllegalArgumentException("CPF inválido: " + cpf);
         }
 
@@ -19,34 +32,18 @@ public class DocumentValid {
             numbers[i] = Character.getNumericValue(cpf.charAt(i));
         }
 
-        int sum1 = 0;
-        for (int i = 0; i < 9; i++) {
-            sum1 += numbers[i] * (10 - i);
-        }
-        int remainder1 = sum1 % 11;
-        int digit1 = (remainder1 < 2) ? 0 : (11 - remainder1);
-
-        int sum2 = 0;
-        for (int i = 0; i < 10; i++) {
-            sum2 += numbers[i] * (11 - i);
-        }
-        int remainder2 = sum2 % 11;
-        int digit2 = (remainder2 < 2) ? 0 : (11 - remainder2);
+        int[] factors1 = {10, 9, 8, 7, 6, 5, 4, 3, 2};
+        int[] factors2 = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+        int digit1 = calculateDigit(numbers, factors1);
+        int digit2 = calculateDigit(numbers, factors2);
 
         return (digit1 == numbers[9]) && (digit2 == numbers[10]);
-
     }
 
     public static boolean isCNPJValid(String cnpj) {
-        // Removes all non-digit characters
-        cnpj = cnpj.replaceAll("[^0-9]", "");
+        cnpj = removeNonDigits(cnpj);
 
-        // CNPJ's formed by a sequence of equal numbers are considered errors
-        if (cnpj.equals("00000000000000") || cnpj.equals("11111111111111") ||
-                cnpj.equals("22222222222222") || cnpj.equals("33333333333333") ||
-                cnpj.equals("44444444444444") || cnpj.equals("55555555555555") ||
-                cnpj.equals("66666666666666") || cnpj.equals("77777777777777") ||
-                cnpj.equals("88888888888888") || cnpj.equals("99999999999999") || (cnpj.length() != 14)) {
+        if (cnpj.length() != 14 || hasRepeatedDigits(cnpj)) {
             throw new IllegalArgumentException("CNPJ inválido: " + cnpj);
         }
 
@@ -55,23 +52,10 @@ public class DocumentValid {
             numbers[i] = Character.getNumericValue(cnpj.charAt(i));
         }
 
-        int sum1 = 0;
-        int factor1 = 5;
-        for (int i = 0; i < 12; i++) {
-            sum1 += numbers[i] * factor1;
-            factor1 = (factor1 == 2) ? 9 : factor1 - 1;
-        }
-        int remainder1 = sum1 % 11;
-        int digit1 = (remainder1 < 2) ? 0 : (11 - remainder1);
-
-        int sum2 = 0;
-        int factor2 = 6;
-        for (int i = 0; i < 13; i++) {
-            sum2 += numbers[i] * factor2;
-            factor2 = (factor2 == 2) ? 9 : factor2 - 1;
-        }
-        int remainder2 = sum2 % 11;
-        int digit2 = (remainder2 < 2) ? 0 : (11 - remainder2);
+        int[] factors1 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        int[] factors2 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        int digit1 = calculateDigit(numbers, factors1);
+        int digit2 = calculateDigit(numbers, factors2);
 
         return (digit1 == numbers[12]) && (digit2 == numbers[13]);
     }
