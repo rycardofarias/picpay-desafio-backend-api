30f4126c784e163e83159169c3766137e2c4c7a6
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-17 02:01:33 -0300

    Add environment variable VAR_AUTHORIZED and VAR_UNAUTHORIZED

--- src/main/java/com/example/picpay/config/variables/EnvironmentVariable.java
+++ src/main/java/com/example/picpay/config/variables/EnvironmentVariable.java
@@ -1,5 +1,8 @@
 package com.example.picpay.config.variables;
 
 public class EnvironmentVariable {
-    public static String VAR_AUTHORIZED = "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6";
+
+    public static String VAR_AUTHORIZED = "https://run.mocky.io/v3/b002059b-2e21-42fd-8287-2a05a3b046f0";
+
+    public static String VAR_UNAUTHORIZED = "https://run.mocky.io/v3/e0cacd34-c8f0-440d-8be5-7e74b73e0198";
 }
