3e46c1f0edf864c1514ecab7a5ffe1fdeec034ff
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-17 19:32:11 -0300

    refactoring exceptions

--- src/main/java/com/example/picpay/services/UserService.java
+++ src/main/java/com/example/picpay/services/UserService.java
@@ -10,6 +10,7 @@ import com.example.picpay.exceptions.messages.ExceptionMessages;
 import com.example.picpay.repository.UserRepository;
 import com.example.picpay.validation.documents.DocumentValid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -24,21 +25,21 @@ public class UserService {
     public void validateTransaction(User sender, BigDecimal amount) throws TransactionValidationException {
 
         if(sender.getUserType() == UserType.MERCHANT) {
-            throw new TransactionValidationException(ExceptionMessages.USER_NOT_AUTHORIZED);
+            throw new TransactionValidationException(ExceptionMessages.USER_NOT_AUTHORIZED, HttpStatus.FORBIDDEN);
         }
 
         if(sender.getBalance().compareTo(amount) < 0) {
-            throw new TransactionValidationException(ExceptionMessages.INSUFFICIENT_FOUNDS);
+            throw new TransactionValidationException(ExceptionMessages.INSUFFICIENT_FOUNDS, HttpStatus.BAD_REQUEST);
         }
     }
     public User findUserById(Long id) throws UserValidationException {
 
         if (id == null || id <= 0) {
-            throw new UserValidationException(ExceptionMessages.INVALID_USER_ID, id);
+            throw new UserValidationException(ExceptionMessages.INVALID_USER_ID, id, HttpStatus.BAD_REQUEST);
         }
 
         return this.userRepository.findUserById(id)
-                .orElseThrow(() -> new UserValidationException(ExceptionMessages.USER_CANNOT_BE_NULL));
+                .orElseThrow(() -> new UserValidationException(ExceptionMessages.USER_CANNOT_BE_NULL, HttpStatus.BAD_REQUEST));
     }
 
     public User createUser(UserDTO userDTO) throws UserValidationException, DocumentValidationException {
@@ -48,7 +49,7 @@ public class UserService {
         return saveUser(user);
     }
 
-    public User saveUser(User user) throws UserValidationException, DocumentValidationException {
+    public User saveUser(User user) {
 
         return this.userRepository.save(user);
     }
@@ -56,19 +57,19 @@ public class UserService {
     private void validateUser(User user) throws UserValidationException, DocumentValidationException {
 
         if (user == null) {
-            throw new UserValidationException(ExceptionMessages.USER_CANNOT_BE_NULL);
+            throw new UserValidationException(ExceptionMessages.USER_CANNOT_BE_NULL, HttpStatus.BAD_REQUEST);
         }
 
         if (userRepository.existsByDocument(user.getDocument())) {
-            throw new DocumentValidationException(ExceptionMessages.USER_DOCUMENT_ALREADY_REGISTERED + user.getDocument());
+            throw new DocumentValidationException(ExceptionMessages.USER_DOCUMENT_ALREADY_REGISTERED + user.getDocument(), HttpStatus.CONFLICT);
         }
 
         if (!DocumentValid.isCPFValid(user.getDocument())) {
-            throw new DocumentValidationException(ExceptionMessages.USER_DOCUMENT_INVALID + user.getDocument());
+            throw new DocumentValidationException(ExceptionMessages.USER_DOCUMENT_INVALID + user.getDocument(), HttpStatus.BAD_REQUEST);
         }
 
         if (userRepository.existsByEmail(user.getEmail())) {
-            throw new UserValidationException(ExceptionMessages.USER_EMAIL_ALREADY_REGISTERED + user.getEmail());
+            throw new UserValidationException(ExceptionMessages.USER_EMAIL_ALREADY_REGISTERED + user.getEmail(), HttpStatus.CONFLICT);
         }
     }
 
